386d57bc105a4db37e234256c7a1b1fa660e0381
Ana Clara <ana.cws1@al.insper.edu.br>
2025-04-29 10:04:08 -0300

    exercicio 2 - correção 2

--- funcoes.py
+++ funcoes.py
@@ -7,10 +7,11 @@ def rolar_dados(n):
         i += 1
     return lista
 
-def guardar_dado (dados_rolados, dados_guardados, número_dado):
-    dado = dados_rolados[número_dado]
-        novos_dados_rolados = dados_rolados[:número_dado] + dados_rolados[número_dado+1:]
-        novos_dados_no_estoque = dados_no_estoque + [dado]
-        return [novos_dados_rolados, novos_dados_no_estoque]
+def guardar_dado(dados_rolados, dados_guardados, numero_dado):
+    if 0 <= numero_dado < len(dados_rolados):
+        dado = dados_rolados[numero_dado]
+        novos_dados_rolados = dados_rolados[:numero_dado] + dados_rolados[numero_dado+1:]
+        novos_dados_guardados = dados_guardados + [dado]
+        return [novos_dados_rolados, novos_dados_guardados]
     else:
-        return [dados_rolados, dados_no_estoque]
+        return [dados_rolados, dados_guardados]
