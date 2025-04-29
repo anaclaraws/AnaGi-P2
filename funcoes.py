f623d7842515bbe7ef8086651d031d1dfaeabeb9
Ana Clara <ana.cws1@al.insper.edu.br>
2025-04-29 09:45:19 -0300

    exercicio 2

--- funcoes.py
+++ funcoes.py
@@ -6,3 +6,9 @@ def rolar_dados(n):
         lista.append(random.randint(1, 6))
         i += 1
     return lista
+
+def guardar_dado (dados_rolados, dados_guardados, número_dado):
+    if 0 <= indice < len(dados_rolados):
+        dado = dados_rolados.pop(número_dado)
+        dados_no_estoque.append(dado)
+    return [dados_rolados, dados_no_estoque]
